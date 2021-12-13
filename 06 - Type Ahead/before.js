bb7653ec4578fdcba120e6656c3fb37a3778b77b
kminzy <kminzy0704@gmail.com>
2021-12-13 10:39:20 +0900

    before, after js파일 분리

---
+++ 06 - Type Ahead/before.js
@@ -0,0 +1,25 @@
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+function loadData() {
+  return fetch(endpoint)
+    .then((response) => response.json())
+    .then((json) => displayData(json));
+}
+
+function displayData(items) {
+  const li = document.createElement("li");
+  const search = document.querySelector(".search");
+  const container = document.querySelector(".suggestions");
+  const mapItem = items.map((item) => ({
+    city: item.city,
+    state: item.state,
+    population: item.population,
+  }));
+
+  if (search.value.includes(`${items}`)) {
+    container.innerHTML = `<li>${mapItem.city}</li>`;
+  }
+}
+
+loadData();
